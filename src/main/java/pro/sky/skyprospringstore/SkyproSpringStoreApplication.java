d22a93200387f5987df033517e830e006d797656
huliopopo <yashin.hlpp@gmail.com>
2024-06-02 16:59:38 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyprospringstore/SkyproSpringStoreApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyprospringstore/SkyproSpringStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproSpringStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
